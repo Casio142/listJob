32556109a6d248d4f187f7b99dc1ac4bcef854af
Sonic <jameeldeleon@Jameels-MacBook-Air.local>
2019-04-21 02:42:34 -0400

    Beta version 0.069 :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import GreetPage from './GreetPage';
+import PostingDisplay from './PostingDisplay';
+import PopupWindow from './PopupWindow';
 
 class App extends Component {
+ // <div dangerouslySetInnerHTML={{ __html: post.description }}/>
+
+  constructor(props){
+
+    super(props);
+    this.state = {
+    posts: [],
+    pageNumber: 1,
+    togglePopup: false}
+    this.addMore = this.addMore.bind(this);
+    this.resetListing = this.resetListing.bind(this);
+    this.jobClick = this.jobClick.bind(this);
+  }
+  
+  /*
+   Func Name: addMore
+   Purpose: Retrieve all the job listings with no filter from the API
+   */
+   addMore(){
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = proxyurl + "//jobs.github.com/positions.json?page="+this.state.pageNumber+"&utf8=%E2%9C%93&description=&location=";
+    axios.get(url, {onDownloadProgress: function(){
+    }}) 
+      .then(res => {
+        this.setState(
+          {
+            posts : [...this.state.posts,...res.data],
+            pageNumber : this.state.pageNumber + 1,
+            togglePopup : false,
+            popWindowInfo: null
+          }
+        )
+        console.log(res);
+      })  
+   }
+
+   jobClick(card){
+    this.setState({
+      togglePopup: !this.state.togglePopup,
+      popWindowInfo: card 
+
+    })
+     console.log("the job click shit");
+   }
+
+   closePopup(){
+     this.setState(
+       {
+         togglePopup: false
+       }
+     )
+   }
+
+   /*
+   Func Name: resetListing
+   Purpose: Erase all the listing that are displayed
+   */
+   resetListing(){
+     this.setState(
+       {
+         posts: [],
+         pageNumber: 1
+       }
+     )
+   }
+
   render() {
+    const {posts,togglePopup} = this.state;
+    var postings = <GreetPage message="NEED A JOB?" btnLabel="CLICK TO SEE SOFTWARE JOB POSTINGS" clickAction={this.addMore}/>;
+    if(togglePopup){
+      var popUp = <PopupWindow toggle={this.jobClick} data = {this.state.popWindowInfo}/>
+    }
+    else{
+      popUp = null;
+    }
+    if(posts.length > 0){
+      postings = (<PostingDisplay jobClick={this.jobClick} posts= {posts} clickAddAction={this.addMore} clickResetAction={this.resetListing} moreBtnLabel="MORE JOB POSTINGS" resetBtnLabel="ERASE ALL LISTINGS"/>)
+    }
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="app-shield">
+          {popUp}
+          {postings}
+        </div>  
       </div>
     );
   }
